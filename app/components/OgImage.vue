0821576dd8ba94959be7eada44efbca161c64bb2
kumkoonDev <kumkoon.production@gmail.com>
2025-10-14 14:52:19 +0700

    update OG Image

--- app/components/OgImage.vue
+++ app/components/OgImage.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="flex flex-col justify-center items-center w-full h-full bg-[#111827] text-white text-center">
-    <h1 class="text-7xl font-bold mb-4 text-[#f4a13f]">{{ title }}</h1>
-    <p class="text-4xl opacity-80">{{ description }}</p>
+  <div
+    class="flex flex-col justify-center items-center w-full h-full bg-[#111827] text-white text-center"
+  >
+    <img
+      src="/imgs/LOGO_KKP.png"
+      alt="logo"
+      class="w-80 h-80 object-contain mx-auto"
+    />
+    <h1 class="text-7xl -top-8 font-bold text-[#f4a13f]">{{ title }}</h1>
+    <p class="text-4xl -top-8 opacity-80">{{ description }}</p>
   </div>
 </template>
 
@@ -9,5 +16,5 @@
 defineProps({
   title: String,
   description: String,
-})
+});
 </script>
